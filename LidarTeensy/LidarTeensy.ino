68bb4d4063f7c97744d42ed4a60c115498c40f54
Carter Turnbaugh <carturn@nuevaschool.org>
2017-02-21 18:40:34 -0800

    Fixed issue with rate not going above 5

--- LidarTeensy/LidarTeensy.ino
+++ LidarTeensy/LidarTeensy.ino
@@ -4,29 +4,29 @@
 
 struct encoderData {
   long lastRead;
-  long position;
+  long pos;
   long rate;
-  Encoder* enc;
-  uint32_t canID;
 };
 
-encoderData leftEncoderData;
-encoderData rightEncoderData;
+Encoder leftEncoder(16, 17);
+encoderData leftData;
+Encoder rightEncoder(18, 19);
+encoderData rightData;
 
 void resetLeftEncoder(byte * msg) {
   if (msg[0] == 0x72 && msg[1] == 0x65 && msg[2] == 0x73 && msg[3] == 0x65 && msg[4] == 0x74 && msg[5] == 0x65 && msg[6] == 0x6e && msg[7] == 0x63) {
-    leftEncoderData.enc->write(0);
-    leftEncoderData.position = 0;
-    leftEncoderData.rate = 0;
+    leftEncoder.write(0);
+    leftData.pos = 0;
+    leftData.rate = 0;
     Serial.println("reset");
   }
 }
 
 void resetRightEncoder(byte * msg) {
   if (msg[0] == 0x72 && msg[1] == 0x65 && msg[2] == 0x73 && msg[3] == 0x65 && msg[4] == 0x74 && msg[5] == 0x65 && msg[6] == 0x6e && msg[7] == 0x63) {
-    rightEncoderData.enc->write(0);
-    rightEncoderData.position = 0;
-    rightEncoderData.rate = 0;
+    rightEncoder.write(0);
+    rightData.pos = 0;
+    rightData.rate = 0;
     Serial.println("reset");
   }
 }
@@ -39,17 +39,13 @@ void setup() {
   pinMode(25, OUTPUT);
   digitalWrite(24, HIGH);
   CAN_begin();
-  leftEncoderData.lastRead = 0;
-  leftEncoderData.position = -999;
-  leftEncoderData.rate = 0;
-  leftEncoderData.enc = new Encoder (16, 17);
-  leftEncoderData.canID = 0x610;
+  leftData.lastRead = 0;
+  leftData.pos = -999;
+  leftData.rate = 0;
   CAN_add_id(0x610, &resetLeftEncoder);
-  rightEncoderData.lastRead = 0;
-  rightEncoderData.position = -999;
-  rightEncoderData.rate = 0;
-  rightEncoderData.enc = new Encoder (18, 19);
-  rightEncoderData.canID = 0x611;
+  rightData.lastRead = 0;
+  rightData.pos = -999;
+  rightData.rate = 0;
   CAN_add_id(0x611, &resetRightEncoder);
 }
 
@@ -70,23 +66,31 @@ void writeLongs(uint32_t id, long value1, long value2) {
   delete msg;
 }
 
-void canEncoder(encoderData encData) {
-  long newPos = encData.enc->read();
-  if (newPos != encData.position) {
-    encData.rate = ((double) 1000000.0 * (newPos - encData.position)) / ((double) (micros() - encData.lastRead));
-    encData.position = newPos;
-    encData.lastRead = micros();
+void loop() {
+  long newPos = leftEncoder.read();
+  if (newPos != leftData.pos) {
+    leftData.rate = ((double) 1000000.0 * (newPos - leftData.pos)) / ((double) (micros() - leftData.lastRead));
+    Serial.println(leftData.rate);
+    leftData.pos = newPos;
+    leftData.lastRead = micros();
   }
   else {
-    if ((micros() - encData.lastRead) > 1000) {
-      encData.rate = 0;
+    if ((micros() - leftData.lastRead) > 1000) {
+      leftData.rate = 0;
     }
   }
-  writeLongs (encData.canID, encData.position, encData.rate);
-}
-
-void loop() {
-  canEncoder (leftEncoderData);
-  canEncoder (rightEncoderData);
+  writeLongs(0x610, leftData.pos, leftData.rate);
+  newPos = rightEncoder.read();
+  if (newPos != rightData.pos) {
+    rightData.rate = ((double) 1000000.0 * (newPos - rightData.pos)) / ((double) (micros() - rightData.lastRead));
+    rightData.pos = newPos;
+    rightData.lastRead = micros();
+  }
+  else {
+    if ((micros() - rightData.lastRead) > 1000) {
+      rightData.rate = 0;
+    }
+  }
+  writeLongs(0x611, rightData.pos, rightData.rate);
   delay(10);
 }
