6916a7951c824548857733b1ba217188e8b373d8
Tobias Braun <tobbrau@nuevaschool.org>
2017-03-25 16:27:15 -0700

    Added Custom Driver Station code, V1 is the current one in use

---
+++ TeensyStick/TeensyStickV1.ino
@@ -0,0 +1,67 @@
+/* Basic USB Joystick Example
+   Teensy becomes a USB joystick
+
+   You must select Joystick from the "Tools > USB Type" menu
+
+   Pushbuttons should be connected to digital pins 0 and 1.
+   Wire each button between the digital pin and ground.
+   Potentiometers should be connected to analog inputs 0 to 1.
+
+   This example code is in the public domain.
+*/
+
+void setup() {
+  pinMode(0, INPUT_PULLUP);
+  pinMode(1, INPUT_PULLUP);
+  pinMode(2, INPUT_PULLUP);
+  pinMode(3, INPUT_PULLUP);
+  pinMode(4, INPUT_PULLUP);
+  pinMode(5, INPUT_PULLUP);
+  pinMode(6, INPUT_PULLUP);
+  pinMode(7, INPUT_PULLUP);
+  pinMode(13, INPUT_PULLUP);
+  pinMode(14, INPUT_PULLUP);
+  pinMode(15, INPUT_PULLUP);
+  pinMode(16, INPUT_PULLUP);
+  pinMode(17, INPUT_PULLUP);
+  pinMode(18, INPUT_PULLUP);
+//  Serial.begin(9600);
+//  while(!Serial){}
+}
+
+void loop() {
+
+  // read the digital inputs and set the buttons
+  Joystick.button(1, not digitalRead(0));
+  Joystick.button(2, not digitalRead(1));
+  Joystick.button(3, not digitalRead(2));
+  Joystick.button(4, not digitalRead(3));
+  Joystick.button(5, not digitalRead(4));
+  Joystick.button(6, not digitalRead(5));
+  Joystick.button(7, not digitalRead(6));
+  Joystick.button(8, not digitalRead(7));
+  Joystick.button(9, not digitalRead(13));
+  Joystick.button(10, not digitalRead(14));
+  Joystick.button(11, not digitalRead(15));
+  Joystick.button(12, not digitalRead(16));
+  Joystick.button(13, not digitalRead(17));
+  Joystick.button(14, not digitalRead(18));
+//  Serial.println(not digitalRead(0));
+//  Serial.println(not digitalRead(1));
+//  Serial.println(not digitalRead(2));
+//  Serial.println(not digitalRead(3));
+//  Serial.println(not digitalRead(4));
+//  Serial.println(not digitalRead(5));
+//  Serial.println(not digitalRead(6));
+//  Serial.println(not digitalRead(7));
+//  Serial.println(not digitalRead(13));
+//  Serial.println(not digitalRead(14));
+//  Serial.println(not digitalRead(15));
+//  Serial.println(not digitalRead(16));
+//  Serial.println(not digitalRead(17));
+//  Serial.println(not digitalRead(18));
+
+  // a brief delay, so this runs 20 times per second
+  delay(50);
+}
+
